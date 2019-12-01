e5bfc55dbe8288ce1fc60b852843bf9882a2a3c0
dodinhnguyen <nguyendo891@yahoo.com>
2019-12-01 15:37:42 +0100

    Add project files.

---
+++ Behvioral.Observer.Event/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Behvioral.Observer.Event
+{
+    public class FallsIllEventArgs
+    {
+        public string Address;
+    }
+
+    public class Person
+    {
+        public void CatchACold()
+        {
+            FallsIll?.Invoke(this,
+              new FallsIllEventArgs { Address = "123 London Road" });
+        }
+
+        public event EventHandler<FallsIllEventArgs> FallsIll;
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+
+            person.FallsIll += CallDoctor;
+
+            person.CatchACold();
+        }
+        private static void CallDoctor(object sender, FallsIllEventArgs eventArgs)
+        {
+            Console.WriteLine($"A doctor has been called to {eventArgs.Address}");
+        }
+    }
+}
